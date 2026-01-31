587c965f8515e988a8706bc735b7297d9aaa3048
Arno Simons <arno.simons@gmail.com>
2026-01-31 13:50:09 +0100

    Remove get_meta_free_dict from imports

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 from .analyse import SequenzAnalyse, SequenzAnalyseErgebnis, analyse
 from .config import SequenzAnalyseConfig
-from .utils import analyse_als_json_speichern, txt_sequenzierung, get_meta_free_dict
+from .utils import analyse_als_json_speichern, txt_sequenzierung, remove_responses_meta
 
 __all__ = [
     "SequenzAnalyse",
