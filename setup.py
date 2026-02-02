14f27e3011a8e666f8a0a19d7e75bdf930aa4961
Arno Simons <arno.simons@gmail.com>
2026-02-02 21:34:53 +0100

    Rename package from sequenzanalyse to sequentialanalyse

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 README = Path(__file__).with_name("README.md").read_text(encoding="utf-8")
 
 setup(
-    name="sequenzanalyse",
+    name="sequentialanalyse",
     version="0.1.0",
     description="Sequential analysis pipeline in the German tradition of Objektive Hermeneutik.",
     long_description=README,
@@ -15,14 +15,14 @@ setup(
     license="",
     python_requires=">=3.10",
     install_requires=["openai"],
-    packages=["sequenzanalyse"],
-    package_dir={"sequenzanalyse": "."},
+    packages=["sequentialanalyse"],
+    package_dir={"sequentialanalyse": "."},
     include_package_data=True,
-    package_data={"sequenzanalyse": ["_prompts/*.txt"]},
+    package_data={"sequentialanalyse": ["_prompts/*.txt"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPLv3 License",
         "Operating System :: OS Independent",
     ],
 )
